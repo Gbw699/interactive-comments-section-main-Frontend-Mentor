a7bc62ce1edc558c7d599246e1994ca496f94c99
Gabriel Barimboim <gdbarimboim@gmail.com>
2024-12-16 12:40:14 -0300

    FEAT retrive data from a local file

--- application/src/app/app.component.ts
+++ application/src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommnetsService } from './services/commnets.service';
+import { CommaExpr } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'application';
+
+  constructor(private comments: CommnetsService) {}
+
+  ngOnInit(): void {
+    this.comments.getComments();
+  }
 }
---
+++ application/src/app/services/commnets.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, signal, WritableSignal } from '@angular/core';
+
+import { IComments } from '../../models/IComments';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommnetsService {
+  private commnets: WritableSignal<IComments[] | undefined> = signal(undefined);
+
+  constructor(private http: HttpClient) {}
+
+  getComments() {
+    this.http
+      .get('http://localhost:4200/assets/data.json')
+      .subscribe((value: any) => {
+        this.commnets.set(value.comments);
+      });
+  }
+}
