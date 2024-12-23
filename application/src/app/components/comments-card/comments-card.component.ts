1a92d4a958f8734bbda916cf78af232625aa370c
Gabriel Barimboim <gdbarimboim@gmail.com>
2024-12-23 15:00:21 -0300

    FEAT detail card for comments

---
+++ application/src/app/components/comments-card/comments-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comments-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './comments-card.component.html',
+  styleUrl: './comments-card.component.scss'
+})
+export class CommentsCardComponent {
+
+}
