ce1ea30f55c90b058962129fd316031db54a7a2c
Gabriel Barimboim <gdbarimboim@gmail.com>
2024-12-23 14:37:55 -0300

    FEAT service to manage current user

---
+++ application/src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, signal, WritableSignal } from '@angular/core';
+import { ICurrentUser } from '../../models/ICurrentUser';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private currentUser: WritableSignal<ICurrentUser | undefined> =
+    signal(undefined);
+
+  constructor(private http: HttpClient) {}
+
+  getComments() {
+    this.http
+      .get('http://localhost:4200/assets/data.json')
+      .subscribe((value: any) => {
+        this.currentUser.set(value.currentUser);
+        console.log(this.currentUser());
+      });
+  }
+}
