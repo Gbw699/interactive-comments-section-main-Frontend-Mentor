d0ba7d9e1af942774443f4fd8a87b1b9230c2f69
Gabriel Barimboim <gdbarimboim@gmail.com>
2024-12-27 12:38:26 -0300

    FEAT comments form 2

--- application/src/app/components/form-card/form-card.component.ts
+++ application/src/app/components/form-card/form-card.component.ts
@@ -1,21 +1,14 @@
-import {
-  Component,
-  computed,
-  OnChanges,
-  Signal,
-  SimpleChanges,
-} from '@angular/core';
-import { UserService } from '../../services/user.service';
-import { ICurrentUser } from '../../../models/ICurrentUser';
-import { computeMsgId } from '@angular/compiler';
+import { Component, computed, Signal } from '@angular/core';
 import {
   FormControl,
   FormGroup,
-  FormsModule,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
+import { UserService } from '../../services/user.service';
+import { ICurrentUser } from '../../../models/ICurrentUser';
 
 @Component({
   selector: 'app-form-card',
@@ -32,16 +25,14 @@ export class FormCardComponent {
   formGroup: FormGroup = new FormGroup({
     inputValue: new FormControl('', [
       Validators.required,
-      Validators.maxLength(50),
+      Validators.maxLength(300),
     ]),
   });
 
   constructor(private user: UserService) {}
 
   publishComment(event: any) {
-    console.log(event.target.elements[0].value);
-
     let result: string = this.formGroup.value.inputValue;
-    // this.formGroup.setValue({ inputValue: '' });
+    this.formGroup.setValue({ inputValue: '' });
   }
 }
