b8df6f75528ee79ec0005d0b2ba3030e11876c65
Gabriel Barimboim <gdbarimboim@gmail.com>
2025-01-06 15:40:04 -0300

    FEAT modal component

---
+++ application/src/app/components/modal-card/modal-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-card.component.html',
+  styleUrl: './modal-card.component.scss'
+})
+export class ModalCardComponent {
+
+}
