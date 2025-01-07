07f02484893e01b95c1d377af09a3d2eb89888a2
Gabriel Barimboim <gdbarimboim@gmail.com>
2025-01-07 12:34:27 -0300

    FEAT new pipe to handle dates

---
+++ application/src/app/pipes/custom-date.pipe.ts
@@ -0,0 +1,67 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'customDate',
+  standalone: true,
+})
+export class CustomDatePipe implements PipeTransform {
+  transform(value: Date): string {
+    let newDate = new Date();
+    let dateFromNow = {
+      year: newDate.getUTCFullYear(),
+      month: newDate.getUTCMonth(),
+      date: newDate.getUTCDate(),
+      day: newDate.getUTCDay(),
+      hours: newDate.getUTCHours(),
+      minutes: newDate.getUTCMinutes(),
+    };
+    let dateToHandle = {
+      year: value.getUTCFullYear(),
+      month: value.getUTCMonth(),
+      date: value.getUTCDate(),
+      day: value.getUTCDay(),
+      hours: value.getUTCHours(),
+      minutes: value.getUTCMinutes(),
+    };
+    let now: any = new Date(
+      Date.UTC(
+        dateFromNow.year,
+        dateFromNow.month,
+        dateFromNow.date,
+        dateFromNow.hours,
+        dateFromNow.minutes
+      )
+    );
+    let targetDate: any = new Date(
+      Date.UTC(
+        dateToHandle.year,
+        dateToHandle.month,
+        dateToHandle.date,
+        dateToHandle.hours,
+        dateToHandle.minutes
+      )
+    );
+
+    const diffMilliseconds = now - targetDate;
+    const diffMinutes = Math.floor(diffMilliseconds / (1000 * 60));
+    const diffHours = Math.floor(diffMilliseconds / (1000 * 60 * 60));
+    const diffDays = Math.floor(diffMilliseconds / (1000 * 60 * 60 * 24));
+    const diffWeeks = Math.floor(diffDays / 7);
+    const diffMonths = Math.floor(diffDays / 30);
+    const diffYears = Math.floor(diffDays / 365);
+
+    if (diffMinutes < 60) {
+      return diffMinutes === 1 ? '1 minute ago' : `${diffMinutes} minutes ago`;
+    } else if (diffHours < 24) {
+      return diffHours === 1 ? '1 hour ago' : `${diffHours} hours ago`;
+    } else if (diffDays < 7) {
+      return diffDays === 1 ? '1 day ago' : `${diffDays} days ago`;
+    } else if (diffWeeks < 4) {
+      return diffWeeks === 1 ? '1 week ago' : `${diffWeeks} weeks ago`;
+    } else if (diffMonths < 12) {
+      return diffMonths === 1 ? '1 month ago' : `${diffMonths} months ago`;
+    } else {
+      return diffYears === 1 ? '1 year ago' : `${diffYears} years ago`;
+    }
+  }
+}
