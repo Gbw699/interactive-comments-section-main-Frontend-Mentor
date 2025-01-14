9c3620a49a4ce3d864de83c309ec6f08981ce6d0
Gabriel Barimboim <gdbarimboim@gmail.com>
2025-01-14 15:54:27 -0300

    REFACTOR from previous interface change

--- application/src/app/services/user.service.ts
+++ application/src/app/services/user.service.ts
@@ -14,7 +14,7 @@ export class UserService {
     this.http
       .get('http://localhost:4200/assets/data.json')
       .subscribe((value: any) => {
-        this.currentUser.set({ currentUser: { ...value.currentUser } });
+        this.currentUser.set({ ...value.currentUser });
         console.log(this.currentUser());
       });
   }
