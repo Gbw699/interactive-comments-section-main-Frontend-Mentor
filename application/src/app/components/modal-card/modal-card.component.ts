e8fe7987b0356dafb158b8fa8c38233b809be6e6
Gabriel Barimboim <gdbarimboim@gmail.com>
2025-01-17 12:41:43 -0300

    REFACTOR change the way to use the state signal inside the component

--- application/src/app/components/modal-card/modal-card.component.ts
+++ application/src/app/components/modal-card/modal-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, Signal } from '@angular/core';
 
 import { DeleteModalService } from '../../services/delete-modal.service';
 import { CommentsService } from '../../services/comments.service';
@@ -11,15 +11,17 @@ import { CommentsService } from '../../services/comments.service';
   styleUrl: './modal-card.component.scss',
 })
 export class ModalCardComponent {
+  commentId: Signal<number | undefined> = computed(() => {
+    return this.deleteModalService.commentIdToDelete();
+  });
+
   constructor(
     private commentsService: CommentsService,
     private deleteModalService: DeleteModalService
   ) {}
 
   deleteComment() {
-    this.commentsService.deleteComment(
-      this.deleteModalService.commentIdToDelete()
-    );
+    this.commentsService.deleteComment(this.commentId());
     this.deleteModalService.setCommentIdToDelete(undefined);
   }
 
